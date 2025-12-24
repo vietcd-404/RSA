6eed838441c2a7c6ee0dc3f1652ea2ce87612ed9
vietcd <vietcd245@gmail.com>
2025-12-25 00:42:58 +0700

    updaet

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.haui.rsa {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+    requires org.apache.pdfbox;
 
 
     opens com.haui.rsa to javafx.fxml;
